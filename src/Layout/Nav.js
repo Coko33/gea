426747612b4d47dc67c0271aaeb6e82d050b2f6e
Coko33 <nicolasplacido@gmail.com>
2024-01-22 09:14:16 -0300

    elem nav #1

---
+++ src/Layout/Nav.js
@@ -0,0 +1,12 @@
+import "./Nav.css"
+
+export default function Nav() {
+    return(
+        <>
+            <div className="nav__cnt">
+                <div className="navLogo__cnt"></div>
+                <div className="navItems__cnt"></div>
+            </div>
+        </>
+    )
+}
